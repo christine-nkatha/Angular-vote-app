dcca819e3380b1fb849521a940572c8ec00bf709
christine-nkatha <nkathachristine456@gmail.com>
2022-04-07 10:18:13 +0300

    upload my project

---
+++ src/app/goal-component/goal-component.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-goal-component',
+  templateUrl: './goal-component.component.html',
+  styleUrls: ['./goal-component.component.css']
+})
+export class GoalComponentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
